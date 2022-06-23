397f9c8bc84e6c7bfbc85ed2bf18ca37e75936fc
ryuma017 <tgcrym01@gmail.com>
2022-06-23 12:13:41 +0900

    change host in adderss

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,8 @@
 use api::startup::Application;
 
 // (仮) 設定をどこから取ってくるかは相談。
-const HOST: [u8; 4] = [127, 0, 0, 1];
-const PORT: u16 = 8080;
+const HOST: [u8; 4] = [0, 0, 0, 0];
+const PORT: u16 = 8000;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
